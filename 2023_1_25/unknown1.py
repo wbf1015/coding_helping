c0fc68def0e971dcb830c9c0496aefc712ab7827
Bofan_Wei <2011395@mail.nankai.edu.cn>
2023-02-01 14:28:30 +0800

    2023-2-1 actually

---
+++ 2023_1_25/unknown1.py
@@ -0,0 +1,7 @@
+alist = [True,5.0,8,False, 3,'c174',6,9]
+for i in range(len(alist)-1, -1, -1):
+    print(i, alist[i])
+
+mylist = [9, 5, 6, 4, 3, 1, 2, 8]
+for i in range(3, 6):
+    print('[', i, ']:', mylist[i])
---
+++ 2023_1_31/untitled-174.py
@@ -0,0 +1,43 @@
+import sys
+
+filename = "klingon-english.txt"
+filemode = "r"
+file = open(filename, filemode)
+new_flie = file.readlines()
+file.close()
+
+dictionary = {}
+consonant = ["b", "ch", "D", "gh", "H", "j", "l", "m", "n", "p", "q", "Q", "r", "S", "t", "v", "w", "y", "'"]
+for i in new_flie:
+    split_list = i.split('|')  # breaking the left and right side part of each line
+    dictionary[split_list[0]] = split_list[1].strip()  # storing in as left side as key and right side as value
+
+for key, value in dictionary.items():
+    print('key: ' + key, end='   ')
+    print('value:  ' + value)
+
+# taking user input until it is valid
+
+while True:
+
+    while True:
+        name = input('Which consonant do you want to work with ? ')
+        if name not in consonant:
+            print('Please enter a valid consonant:  ')
+        else:
+            break
+
+    # finding the key that starts with the given consonant
+    # for i in dictionary:
+    #     if i.startswith(name):
+    #         word = dictionary[i]
+    #         key = i
+
+    for key, value in dictionary.items():
+        if key.startswith(name):
+            s = input('How do you translate '+value+'to Klingon?  ')
+            if s == key:
+                print('correct')
+                sys.exit()  # 我觉得题目是这个意思 就是正确了才能退出，如果我理解错了你再改改
+            else:
+                print('Sorry, you’re wrong! The correct answer is ' + key)
